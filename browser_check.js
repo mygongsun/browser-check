9134719dd60db28fab6356f8e20541c720627be3
mygongsun <honorxp@126.com>
2018-05-15 15:58:02 +0800

    add 360 browser support & platform support

--- browser_check.js
+++ browser_check.js
@@ -18,11 +18,42 @@ var BrowserCheck = (function() {
     };
 
     /**
+     * 获取360浏览器和Chrome区别特性
+     */
+     var _mime = function(option, value) {
+     	var mimeTypes = navigator.mimeTypes;
+     	for(var mt in mimeTypes) {
+     		if(mimeTypes[mt][option] == value) {
+     			return true;
+     		}
+     	}
+     	return false;
+     }
+
+    /**
      * 获取完成浏览器类型/版本对象
      */
     BrowserCheck.prototype.get = function() {
 
 		var s = null;
+
+		if(this.ua.match(/android/)) {
+			this.browser.platform = "android";
+		} else if(this.ua.match(/iphone/)) {
+			this.browser.platform = "iphone";
+		} else if(this.ua.match(/ipad/)) {
+			this.browser.platform = "ipad";
+		} else if(this.ua.match(/ipod/)) {
+			this.browser.platform = "ipod";
+		} else if(this.ua.match(/windows/)) {
+			this.browser.platform = "windows";
+		} else if(this.ua.match(/mac/)) {
+			this.browser.platform = "mac";
+		} else if(this.ua.match(/linux/)) {
+			this.browser.platform = "linux";
+		} else {
+			this.browser.platform = "other";
+		}
 
 		if(s = this.ua.match(/edge\/([\d.]+)/)) {
 			this.browser.type = "edge";
@@ -47,7 +78,11 @@ var BrowserCheck = (function() {
 		} else if(s = this.ua.match(/firefox\/([\d.]+)/)) {
 			this.browser.type = "firefox";
 		} else if(s = this.ua.match(/chrome\/([\d.]+)/)) {
-			this.browser.type = "chrome";
+			if(_mime("type", "application/vnd.chromium.remoting-viewer")) {
+				this.browser.type = "360";
+			} else {
+				this.browser.type = "chrome";
+			}
 		} else if(s = this.ua.match(/version\/([\d.]+).*safari/)) {
 			this.browser.type = "safari";
 		} else {
